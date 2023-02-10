f05ed915968dda96cd99b2d18d14c11724f39655
MinAKov <49945726+ColdEiler@users.noreply.github.com>
2023-02-10 21:01:14 +0300

    прочитал задание)

--- lab1/task_3/3.rb
+++ lab1/task_3/3.rb
@@ -18,24 +18,18 @@ def find_first_pos_el(arr)
   -1
 end
 
-def task()
-  p "Введите путь к файлу"
-  filepath = gets
-  file = File.open("#{filepath.chomp}")
-  data = file.readline.split(' ').map{|item|item.to_i }
-  p "Массив: #{data}"
-  p "Выберете функцию 1 или 2 "
-
-  p " 1 - минимум массива"
-  p "2 - индекс первого положительного элемента"
-  choice = gets.chomp
-  case choice
-  when "1"
-    p find_min(data)
-  when "2"
-    p find_first_pos_el(data)
-  else
-    p "Чел, ты не справился"
-    end
+choice = ARGV[0]
+filepath = ARGV[1]
+file = File.open("#{filepath}")
+data = file.readline.split(' ').map{|item|item.to_i }
+p "Массив: #{data}"
+p "Вы Выбрали функцию #{choice} "
+case choice
+when "1"
+  p find_min(data)
+when "2"
+  p find_first_pos_el(data)
+else
+  p "Чел, ты не справился"
 end
-task()
+
