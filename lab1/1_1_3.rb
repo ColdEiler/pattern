f0ed7258bd92195654550e906e4902f16242b738
MinAKov <49945726+ColdEiler@users.noreply.github.com>
2023-02-10 21:00:32 +0300

    ошибки исправил

--- lab1/1_1_3.rb
+++ lab1/1_1_3.rb
@@ -1,11 +1,9 @@
 user = ARGV[0]
-input = STDIN.gets
-name = input.chomp
 
-puts "#{name}, введи команду Ruby: "
+puts "#{user}, введи команду Ruby: "
 ruby_com = STDIN.gets.chomp
 system "ruby -e \"#{ruby_com}\""
 
-puts "#{name}, введи команду ОС: "
+puts "#{user}, введи команду ОС: "
 os_com = STDIN.gets.chomp
 system "#{os_com}"
