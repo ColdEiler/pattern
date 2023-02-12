754b058910ac9d4bce6c0a78f83e87167b06576f
MinAKov <49945726+ColdEiler@users.noreply.github.com>
2023-02-12 21:55:25 +0300

    вот

--- lab1/1_1_3.rb
+++ lab1/1_1_3.rb
@@ -1,8 +1,8 @@
 user = ARGV[0]
 
 puts "#{user}, введи команду Ruby: "
-ruby_com = STDIN.gets.chomp
-system "ruby -e \"#{ruby_com}\""
+ruby_com = STDIN.gets.chomp.downcase
+system "ruby -e '#{ruby_com}''
 
 puts "#{user}, введи команду ОС: "
 os_com = STDIN.gets.chomp
