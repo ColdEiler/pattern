8e575c2335533ffcc3f4513c17d191d3f4545748
MinAKov <49945726+ColdEiler@users.noreply.github.com>
2023-03-05 23:03:24 +0300

    Создание класса student_short

--- lab2/main.rb
+++ lab2/main.rb
@@ -1,13 +1,10 @@
 require_relative 'student'
 require_relative 'student_short.rb'
-
 student_1 = Student.new(lastname: 'Минаков ',firstname: 'Владислав',father_name: 'Андреевич', id: 1, phone:'79667665060')
 #student_2 = Student.new(lastname: 'Титов', firstname: 'Кирилл',father_name: 'Вадимович',id: 2, git:'waasabi13', phone:'79667665060')
 #student_3 = Student.new('Головий','Виталий','Александрович',{git:'https://github.com/VitalijGolovi',id:3})
 #a = student_1.to_json
 #student_3 = Student.from_json(a)
 #puts student_3
-#puts student_3.get_info
-#student_1.from_json('{"lastname":"Минаков","firstname":"Владислав","father_name":"Андреевич", "id": 1, "phone":"79667665060"}')
-# st = Student_short.from_student(student_1)
-# puts st
+st = Student_short.from_student(student_1)
+puts st
--- lab2/student_short.rb
+++ lab2/student_short.rb
@@ -1,32 +1,40 @@
+require 'json'
 require_relative 'student_abstract'
 
 class Student_short<Student_basis
-  attr_accessor :lastname_initials,:contact,:id,:git
+  # attr_accessor :lastname_initials,:contact,:id,:git
+
 
   public_class_method :new
+  private
+  attr_writer :lastname_initials,:contact,:id,:git
+
+  public
+  attr_reader :lastname_initials,:contact,:id,:git
   def self.from_student(student)
     raise ArgumentError,"Нет ID" if student.id.nil?
     Student_short.new(student.id, student.get_info)
   end
 
   def initialize(id,str)
-    options = JSON.parse(str).transform_keys(& to_sym)
+    options = JSON.parse(str).transform_keys(&:to_sym)
     raise ArgumentError, "Нет ФИО!!!" if options[:lastname_initials].nil?
 
     self.id = id
     self.lastname_initials = options[:lastname_initials]
     self.contact = options[:contact]
     self.git = options[:git]
-
-    super(id:self.id,git:self.git)
+    phone = options["phone"]
+    email = options["email"]
+    telegram = options["telegram"]
+    super(id:self.id,git:self.git,phone:phone,email:email,telegram:telegram)
   end
 
   def to_s
     res = lastname_initials
-    res += id
-    [:contact,:git].each do |option|
+    [:contact,:git,:id].each do |option|
       opt_val = send(option)
-      res+= "#{option}: #{opt_val}" unless opt_val.nil?
+      res+= " #{option}: #{opt_val}" unless opt_val.nil?
     end
     res
   end
