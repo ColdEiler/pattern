2376509f11e0821991c51e8457b0f0f35ba70b38
MinAKov <49945726+ColdEiler@users.noreply.github.com>
2023-03-16 01:42:25 +0300

    удалил реализацию to_s для student_short

--- lab2/student_short.rb
+++ lab2/student_short.rb
@@ -1,35 +1,27 @@
 require 'json'
 require_relative 'student_abstract'
 
+
 class Student_short<Student_basis
   # attr_accessor :lastname_initials,:contact,:id,:git
-
-
   public_class_method :new
   attr_reader :lastname_initials,:id,:git,:contact
 
 
   def self.from_student(student)
-      raise ArgumentError,"Нет ID" if student.id.nil?
-      Student_short.new(student.id, student.get_info)
+    raise ArgumentError,"Нет ID" if student.id.nil?
+    Student_short.new(student.id, student.get_info)
+    to_s = student.get_info
   end
 
-    def initialize(id,str)
-      options = JSON.parse(str).transform_keys(&:to_sym)
-      raise ArgumentError, "Нет ФИО!!!" if options[:lastname_initials].nil?
-      @id = id
-      @lastname_initials = options[:lastname_initials]
-      @git = options[:git]
-      @contact = options[:contact]
-    end
+  def initialize(id,str)
+    options = JSON.parse(str).transform_keys(&:to_sym)
+    raise ArgumentError, "Нет ФИО!!!" if options[:lastname_initials].nil?
+    @id = id
+    @lastname_initials = options[:lastname_initials]
+    @git = options[:git]
+    @contact = options[:contact]
 
-
-    def to_s
-      res = "lastname_initials: #{lastname_initials}"
-      [:id,:contact,:git].each do |option|
-        opt_val = send(option)
-        res+= " #{option}:#{opt_val} " unless opt_val.nil?
-      end
-      res
-    end
   end
+
+end
