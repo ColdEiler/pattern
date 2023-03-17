8160e51013a2846f2baa43e3a7f19f50fa0555fc
MinAKov <49945726+ColdEiler@users.noreply.github.com>
2023-03-17 03:25:26 +0300

    Возвращение одобренного

--- lab2/student_abstract.rb
+++ lab2/student_abstract.rb
@@ -1,42 +1,42 @@
-class Student_basis
-  private_class_method :new
-  def contact?
-    !email.nil? || !phone.nil? || !telegram.nil?
+  class Student_basis
+    private_class_method :new
+    def contact?
+      !email.nil? || !phone.nil? || !telegram.nil?
+    end
+    def validate?
+      !git.nil? && contact?
+    end
+
+
+    protected
+    attr_reader :phone, :telegram, :email, :contact
+
+    public
+    attr_reader :id
+    attr_reader :git
+    def initialize( id:nil, phone:nil, telegram:nil, email:nil, git:nil,contact:nil)
+      self.id= id
+      @phone = phone
+      @telegram = telegram
+      @email = email
+      @contact = get_contacts
+      self.git = git
+
+    end
+    def get_contacts
+      return " telegram:#{telegram}" unless telegram.nil?
+      return " email: #{email}" unless email.nil?
+      return " phone: #{phone}" unless phone.nil?
+      nil
+    end
+    # Метод возвращающий один из контактов связи либо nil
+
+    #def git=(git)
+    # raise ArgumentError,"Некорретный ввод: git = #{git} !!!" unless Student_basis.is_val_git?(git) || git.nil?
+    # @git = git
+    #end
+    def id=(id)
+      raise ArgumentError,"Некорретный ввод: id = #{id} !!!" if id == Integer and id < 0
+      @id = id
+    end
   end
-  def validate?
-    !git.nil? && contact?
-  end
-
-
-  protected
-  attr_reader :phone, :telegram, :email
-
-  public
-  attr_reader :id
-  attr_accessor :git
-  def initialize( id:nil, phone:nil, telegram:nil, email:nil, git:nil)
-    self.id= id
-
-    @phone = phone
-    @telegram = telegram
-    @email = email
-    self.git = git
-
-  end
-  def get_contacts
-    return " telegram:#{telegram}" unless telegram.nil?
-    return " email: #{email}" unless email.nil?
-    return " phone: #{phone}" unless phone.nil?
-    nil
-  end
-  # Метод возвращающий один из контактов связи либо nil
-
-  #def git=(git)
-  # raise ArgumentError,"Некорретный ввод: git = #{git} !!!" unless Student_basis.is_val_git?(git) || git.nil?
-  # @git = git
-  #end
-  def id=(id)
-    raise ArgumentError,"Некорретный ввод: id = #{id} !!!" if id == Integer and id < 0
-    @id = id
-  end
-end
--- lab2/student_short.rb
+++ lab2/student_short.rb
@@ -1,13 +1,10 @@
 require 'json'
 require_relative 'student_abstract'
 
-
 class Student_short < Student_basis
   public_class_method :new
-  public 'get_contacts'
+  attr_reader :lastname_initials,:id,:git,:contact
 
-
-  attr_reader :lastname_initials
 
   def self.from_student(student)
     raise ArgumentError,"Нет ID" if student.id.nil?
@@ -18,24 +15,9 @@ class Student_short < Student_basis
   def initialize(id,str)
     options = JSON.parse(str).transform_keys(&:to_sym)
     raise ArgumentError, "Нет ФИО!!!" if options[:lastname_initials].nil?
-    self.id = id
+    @id = id
     @lastname_initials = options[:lastname_initials]
-    self.git = options[:git]
-    params ={}
-    params[:git] = git
-    params[:id] = id
-    con = options[:contact].split(":")
-
-    if con[0] = "phone" then params[:phone] = con[1]
-    elsif con[0]="telegram" then params[:telegram] = con[1]
-    elsif con[0] = "email" then params[:email] = con[1]
-    end
-    super(**params)
-
+    @git = options[:git]
+    @contact = options[:contact]
   end
-
-  def short_contact
-    get_contacts
-  end
-
 end
