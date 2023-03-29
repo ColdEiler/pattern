33fe2b77dcf237d62e8e6ff33b829cf2e0acf31c
MinAKov <49945726+ColdEiler@users.noreply.github.com>
2023-03-29 22:41:29 +0300

    Создание скриптов для БД

---
+++ lab2/db/scripts/insert.sql
@@ -0,0 +1,6 @@
+insert into student(lastname, firstname, father_name, phone, telegram, email, git) values
+                                                                                     ('Минаков', 'Владислав', 'Андреевич', '79667665060', NULL, 'valdos777m@gmail.com', 'ColdEiler'),
+                                                                                     ('Головий', 'Виталий', 'Александрович', NULL, '@vitalijG', NULL, 'vitalijG'),
+                                                                                     ('Титов', 'Кирилл', 'Вадимович', '79002533912', NULL, NULL, 'waasabi13'),
+                                                                                     ('Манукьян', 'Артур', 'Арчибальдович', '79186666666', '@archi', NULL, 'idk-artur-git'),
+                                                                                     ('Анищенко', 'Артур', 'Екатиринович', '79001002030', '@rmyauk', NULL, NULL);
