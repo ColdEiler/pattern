61125f590cafc51b55f781f2fd6300ff608020c5
MinAKov <49945726+ColdEiler@users.noreply.github.com>
2023-03-31 21:22:53 +0300

    доступ к бд

--- Gemfile
+++ Gemfile
@@ -1,2 +1,7 @@
+# frozen_string_literal: true
+
 source "https://rubygems.org"
+
+#gem "rails"
+#gem "sqlite3"
 gem "mysql2"
--- lab2/db/scripts/create.sql
+++ lab2/db/scripts/create.sql
@@ -1,11 +1,11 @@
 create table student (
-                         id          int PRIMARY KEY auto_increment,
-                         lastname   varchar(128) not null,
-                         firstname  varchar(128) not null,
-                         father_name varchar(128) not null,
-                         phone       varchar(20) null,
-                         telegram    varchar(100) null,
-                         email       varchar(100) null,
-                         git         varchar(100) null,
+    id          int PRIMARY KEY auto_increment,
+    lastname   varchar(128) not null,
+    firstname  varchar(128) not null,
+    father_name varchar(128) not null,
+    phone       varchar(20) null,
+    telegram    varchar(100) null,
+    email       varchar(100) null,
+    git         varchar(100) null
 
 );
