a22223e02a4045568b4328672fd596a0f10a4b5f
MinAKov <49945726+ColdEiler@users.noreply.github.com>
2023-03-31 23:21:28 +0300

    student_list_db готов

--- lab2/main.rb
+++ lab2/main.rb
@@ -14,7 +14,7 @@ require 'json'
 
 # student_1 = Student.new(lastname: 'Минаков ',firstname: 'Владислав',father_name: 'Андреевич', id: 1,git:"ColdEiler", phone:'79667665060')
 # student_2 = Student.new(lastname: 'Титов', firstname: 'Кирилл',father_name: 'Вадимович',id: 2, git:'waasabi13', phone:'79667665060')
-# student_3 = Student.new(lastname: 'Головий',firstname: 'Виталий',father_name: 'Александрович',git:'vitaliG',id:3,phone:"79002623817")
+student_3 = Student.new(lastname: 'Головий',firstname: 'Виталий',father_name: 'Александрович',git:'vitaliG',id:3,phone:"79002623817")
 
 # puts 'Тест подключения к БД:'
 
@@ -28,3 +28,8 @@ puts 'Тест работы класса db'
 client = Student_list_db.new("C:/Users/Владислав/OneDrive - FGBOU VO Kubanskiy Gosudarstvennyi Universitet/Рабочий стол/labs/lab2/student.db")
 st = client.student_by_id(1)
 puts st
+
+#client.add_student(student_3)
+#client.delete_student_by_id(6)
+#client.delete_student_by_id(7)
+#puts client.count
--- lab2/student_list/student_list_db.rb
+++ lab2/student_list/student_list_db.rb
@@ -1,5 +1,6 @@
 require 'sqlite3'
 
+
 class Student_list_db
 
     def from_array_to_hash(arr)
@@ -11,6 +12,11 @@ class Student_list_db
         end
         attrs
     end
+
+    def student_fields(student)
+         [student.lastname, student.firstname, student.father_name, student.phone,student.telegram, student.email, student.git]
+    end
+
     
     def initialize(filepath)
         self.client = SQLite3::Database.open filepath
@@ -21,11 +27,36 @@ class Student_list_db
         Student.new(**from_array_to_hash(hash))
     end
 
-    # def add_student(student)
-    #     new_student = client.prepare "Insert into student(lastname,firstname,fathername,phone,telegram,email,git)
-    #         values(?,?,?,?,?,?,?)"
-    # end
+    def add_student(student)
+        command = client.prepare('INSERT INTO student(lastname, firstname, father_name, phone, telegram, email, git) VALUES (?, ?, ?, ?, ?, ?, ?)')
+        command.execute(*student_fields(student))
+    end
 
+    def replace_student_by_id(id,student)
+        command = client.prepare('UPDATE student SET lastname=?, firstname=?, father_name=?, phone=?, telegram=?, email=?, git=? WHERE id=?')
+        command.execute(*student_fields(student),id)
+    end 
+
+    def delete_student_by_id(id)
+        command = client.prepare('DELETE FROM student WHERE id = ?')
+        command.execute(id)
+    end
+
+    def count
+        command = client.execute('select count(id) from student')
+    end
+
+    def get_k_n_student_shorts(k,n, ex_data_list:nil)
+        offset = (k - 1) * n
+        students = client.prepare('SELECT * FROM student LIMIT ?, ?').execute(offset, n)
+        slice = students.map { |h| Student_short.from_student(Student.new(**from_array_to_hash(h))) }
+        return Data_list_Student_Short.new(slice) if ex_data_list.nil?
+
+        ex_data_list.append(*slice)
+        ex_data_list
+    end
+    
+    
     private
 
     attr_accessor :client
