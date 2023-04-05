d6491576ceecff6d94c4a2ac14369bf578215db9
MinAKov <49945726+ColdEiler@users.noreply.github.com>
2023-04-05 19:53:38 +0300

    проверка работы

--- lab2/main.rb
+++ lab2/main.rb
@@ -8,34 +8,31 @@ require_relative 'student_list/students_list_YAML'
 require_relative 'student_list/student_list_txt'
 require_relative 'student_list/Student_list_Base'
 require_relative 'student_list/student_list_db'
+require_relative 'student_list/student_client'
+require_relative 'student_list/File_adapter'
 require 'sqlite3'
 require 'yaml'
 require 'json'
 
 # student_1 = Student.new(lastname: 'Минаков ',firstname: 'Владислав',father_name: 'Андреевич', id: 1,git:"ColdEiler", phone:'79667665060')
 # student_2 = Student.new(lastname: 'Титов', firstname: 'Кирилл',father_name: 'Вадимович',id: 2, git:'waasabi13', phone:'79667665060')
-student_3 = Student.new(lastname: 'Головий',firstname: 'Виталий',father_name: 'Александрович',git:'vitaliG',id:3,phone:"79002623817")
+#student_3 = Student.new(lastname: 'Головий',firstname: 'Виталий',father_name: 'Александрович',git:'vitaliG',id:3,phone:"79002623817")
 
-# puts 'Тест подключения к БД:'
+def test(client)
+    puts client.student_count
 
-# client = SQLite3::Database.open "student.db"
+    puts client.student_by_id(4)
+end
 
-# results = client.query("SELECT * FROM student")
-# results.each { |row| puts row }
+puts "File test (JSON)"
 
-puts 'Тест работы класса db'
+client_json = Student_client.new(FileAdapter.new(Student_List_JSON.new,'result_files/students.json'))
 
-#client = Student_list_db.new("C:/Users/Владислав/OneDrive - FGBOU VO Kubanskiy Gosudarstvennyi Universitet/Рабочий стол/labs/lab2/student.db")
-#client2 = Student_list_db.new("C:/Users/Владислав/OneDrive - FGBOU VO Kubanskiy Gosudarstvennyi Universitet/Рабочий стол/labs/lab2/student.db")
+test(client_json)
 
-client = Student_list_db.instance("C:/Users/Владислав/OneDrive - FGBOU VO Kubanskiy Gosudarstvennyi Universitet/Рабочий стол/labs/lab2/student.db")
-#client2 = Student_list_db.instance("C:/Users/Владислав/OneDrive - FGBOU VO Kubanskiy Gosudarstvennyi Universitet/Рабочий стол/labs/lab2/student.db")
 
-st = client.student_by_id(1)
-puts st
+puts "File test DB"
 
-#client.add_student(student_3)
-#client.delete_student_by_id(6)
-#client.delete_student_by_id(7)
-puts client.count
+client_db = Student_client.new(Student_list_db.instance("C:/Users/Владислав/OneDrive - FGBOU VO Kubanskiy Gosudarstvennyi Universitet/Рабочий стол/labs/lab2/student.db"))
 
+test(client_db)
