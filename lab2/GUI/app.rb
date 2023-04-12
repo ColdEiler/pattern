3bc0a099453e6c6fc49b718b8f3358a0a4855f33
MinAKov <49945726+ColdEiler@users.noreply.github.com>
2023-04-12 15:11:07 +0300

    первая секция готова (пока говнокод)

--- lab2/GUI/app.rb
+++ lab2/GUI/app.rb
@@ -2,36 +2,61 @@
 require 'fox16'
 include Fox
 
-class TabStudents < FXTabItem
-  
-end
-
-class Tab2
-
-end
-
-class Tab3
-
-end
-
-
 class HelloWindow < FXMainWindow
+    def create_label(parent,text,x,y)
+      FXLabel.new(parent, text , :opts => LAYOUT_EXPLICIT,
+        :x => x, :y => y,
+        :width => 50, :height => 20)
+    end
+    
+    def create_text(parent,x,y)
+      FXText.new(parent,:opts => LAYOUT_EXPLICIT,
+        :x => x, :y => y,
+        :width => 200, :height => 20)
+    end
+
+    def create_combobox(parent,x,y)
+      FXComboBox.new(parent,40,:opts => LISTBOX_NORMAL|LAYOUT_EXPLICIT, :x => x, :y => y,
+        :width => 200, :height => 20)
+    end
+
     def initialize(app)
-      super(app, "Hello, World!", :width => 800, :height => 600)
+      super(app, "Hello, World!", :width => 1200, :height => 600)
       tabbook = FXTabBook.new(self, :opts => LAYOUT_FILL) 
       student_tab = FXTabItem.new(tabbook, " Students ")
       student_page = FXVerticalFrame.new(tabbook,:opts => FRAME_RAISED|LAYOUT_FILL)
-      frame = FXVerticalFrame.new(student_page, :opts => LAYOUT_FILL)
-      FXLabel.new(frame, "Source Text:" , :opts => LAYOUT_FILL_X)
-      source_text = FXText.new(frame, :opts => LAYOUT_FILL)
-      FXLabel.new(frame, "Translated text:" , :opts => LAYOUT_FILL_X)
-      translated_text = FXText.new(frame, :opts => TEXT_READONLY|LAYOUT_FILL)
-      controls = FXHorizontalFrame.new(frame, :opts => LAYOUT_FILL_X)
-      FXLabel.new(controls, "Translate from:" )
-      translations = FXComboBox.new(controls, 15,
-      :opts => COMBOBOX_STATIC|FRAME_SUNKEN|FRAME_THICK)
-      translate_button = FXButton.new(controls, "Translate" ,
-      :opts => BUTTON_NORMAL|LAYOUT_RIGHT) 
+      frame = FXVerticalFrame.new(student_page, :opts => LAYOUT_LEFT)
+      
+      create_label(frame, "ФИО", 0,0)
+      fio_text = create_text(frame,0,20)
+      
+      state_names = ['Нет','Да','Неважно'] 
+      
+      
+      create_label(frame,"Email",0,50)
+      email_combobox = create_combobox(frame,0,70) 
+      state_names.each { |name| email_combobox.appendItem(name)}
+      email_text = create_text(frame,0,100)
+      email_text.editable = false
+      check_combobox(email_combobox,email_text)
+
+      create_label(frame,"git",0,120)
+      git_combobox = create_combobox(frame,0,140)
+      state_names.each { |name| git_combobox.appendItem(name)}
+      git_text = create_text(frame,0,170)
+      check_combobox(git_combobox,git_text)
+
+      create_label(frame,"Телефон",0,190)
+      phone_combobox = create_combobox(frame,0,210)
+      state_names.each { |name| phone_combobox.appendItem(name)}
+      phone_text = create_text(frame,0,240)
+      check_combobox(phone_combobox,phone_text)
+
+      create_label(frame,"Телеграм",0,260)
+      teleg_combobox = create_combobox(frame,0,280)
+      state_names.each { |name| teleg_combobox.appendItem(name)}
+      teleg_text = create_text(frame,0,310)
+      check_combobox(teleg_combobox,teleg_text)
       
       
       
@@ -40,6 +65,14 @@ class HelloWindow < FXMainWindow
       extras_tab = FXTabItem.new(tabbook, " Tab2 ")
       extras_page = FXVerticalFrame.new(tabbook,:opts => FRAME_RAISED|LAYOUT_FILL)
     end
+    
+    def check_combobox(combobox,text)
+      combobox.connect(SEL_COMMAND) do |sender, sel, index|
+        text.editable = false if sender.currentItem !=1
+        text.editable = true if sender.currentItem == 1 
+      end      
+    end
+
     def create
       super
       show(PLACEMENT_SCREEN)
