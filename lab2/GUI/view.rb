e9900538097d0dab35a949754a584e0290f34e88
MinAKov <49945726+ColdEiler@users.noreply.github.com>
2023-04-27 12:38:34 +0300

    изменения в view

--- lab2/GUI/view.rb
+++ lab2/GUI/view.rb
@@ -1,10 +1,12 @@
 # frozen_string_literal: true
+
+require_relative '../controller/controller.rb'
 require 'fox16'
 include Fox
-require_relative 'controller.rb'
 
 
 class HelloWindow < FXMainWindow
+  attr_accessor :table
     def create_label(parent,text,x,y)
       FXLabel.new(parent, text , :opts => LAYOUT_EXPLICIT,
         :x => x, :y => y,
@@ -27,15 +29,15 @@ class HelloWindow < FXMainWindow
         :width=>40,:height=>25)
     end
 
-    def sort_columns(table,row_num)
+    def sort_columns(table)
       initials =[]
-      (0..(row_num-1)).each do |row|
-        initials << table.getItemText(row,0)
+      (0..(@row_num-1)).each do |row|
+        initials << table.getItemText(row,1)
       end
       initials.to_a
-      initials = initials.sort_by{|a| a.size}
+      initials = initials.sort_by{|a| a}
       (0..initials.size-1).each do |i|
-        table.setItemText(i,0,initials[i]) 
+        table.setItemText(i,1,initials[i]) 
       end
     end
 
@@ -46,14 +48,13 @@ class HelloWindow < FXMainWindow
       @count_student = 0
       @student_on_table = 20
       @currentpage = 1 
-      @controller =  Controller.new(self)
-
 
       tabbook = FXTabBook.new(self, :opts => LAYOUT_FILL) 
       student_tab = FXTabItem.new(tabbook, " Students ")
       student_page = FXVerticalFrame.new(tabbook,:opts => FRAME_RAISED|LAYOUT_FILL)
       frame = FXVerticalFrame.new(student_page, :opts => LAYOUT_LEFT)
       
+
       create_label(frame, "ФИО", 0,0)
       fio_text = create_text(frame,0,20)
       
@@ -88,13 +89,17 @@ class HelloWindow < FXMainWindow
       teleg_text.editable = false
       check_combobox(teleg_combobox,teleg_text)
       
-      table = FXTable.new(frame,:opts =>LAYOUT_EXPLICIT|TABLE_NO_ROWSELECT|TABLE_NO_COLSELECT|TABLE_ROW_RENUMBER,:x=>250,:y=>0,:height=>400,:width=>470)
-      table.setTableSize(20,3)
-      table.tableStyle |=TABLE_ROW_SIZABLE|TABLE_COL_SIZABLE 
-      table.setColumnText(0, "ФИО")
-      table.setColumnText(1, "Contact")
-      table.setColumnText(2, "Git")
-      table.editable = false
+      self.table = FXTable.new(frame,:opts =>LAYOUT_EXPLICIT|TABLE_NO_ROWSELECT|TABLE_NO_COLSELECT|TABLE_ROW_RENUMBER,:x=>250,:y=>0,:height=>400,:width=>600)
+      self.table.setTableSize(20,4)
+      self.table.tableStyle |=TABLE_ROW_SIZABLE|TABLE_COL_SIZABLE 
+      self.table.setColumnText(0, "ID")
+      self.table.setColumnText(1, "ФИО")
+      self.table.setColumnText(2, "Git")
+      self.table.setColumnText(3, "Contact")
+      self.table.editable = false
+
+      @controller =  Controller.new(self)
+      @controller.refresh_data(1)
 
 
       btn_back=FXButton.new(frame, "Назад", :opts=> BUTTON_INITIAL|LAYOUT_EXPLICIT, :x=>250,:y=>420,:height=>30,:width=>40)
@@ -106,28 +111,6 @@ class HelloWindow < FXMainWindow
       btn_next.textColor = Fox.FXRGB(0,23,175)
 
 
-      ex = [
-        ["Минаков В.А","valdos777m@gmail.com","ColdEiler"],
-        ["Иванов В.А","79667665060","Coler"],
-        ["Власовенко О.А","vlasovOleg@gmail.com","Vlasik"]
-      ]
-      row_num = ex.length
-
-      (0..ex.length-1).each do |i|
-        (0..ex[i].length-1).each do |j|
-          table.setItemText(i,j,ex[i][j])
-          table.setItemJustify(i,j, FXTableItem::CENTER_X)
-        end
-      end
-
-      btn_next.connect(SEL_COMMAND) do
-        table.killSelection
-      end
-
-      
-      btn_back.connect(SEL_COMMAND) do
-        table.killSelection
-      end
 
       create_button = FXButton.new(frame,"Добавить",:opts => LAYOUT_EXPLICIT|BUTTON_NORMAL,:x=>1000,:y=>50,
         :width=>80,:height=>25)
@@ -140,26 +123,24 @@ class HelloWindow < FXMainWindow
         :width=>80,:height=>25)
       edit_button.enabled = false      
 
-      
       button_sort = FXButton.new(frame,"Сортировка ФИО ",:opts =>LAYOUT_EXPLICIT|BUTTON_NORMAL,:x=>1000,:y=>250,:height=>20,:width=>130)
 
-      
       button_sort.connect(SEL_COMMAND) do |sender, sel, pos|
-         sort_columns(table,ex.length) 
+         sort_columns(table) 
       end
 
-      table.connect(SEL_COMMAND) do 
-          if get_select_rows(table) == 0 then 
+      self.table.connect(SEL_COMMAND) do 
+          if get_select_rows() == 0 then 
             edit_button.enabled = true
             delete_button.enabled = true
           end
 
-          if get_select_rows(table) > 0 then
+          if get_select_rows() > 0 then
             delete_button.enabled = true
             edit_button.enabled = false
           end
 
-          if get_select_rows(table) < 0 then 
+          if get_select_rows() < 0 then 
             delete_button.enabled = false
             edit_button.enabled = false
           end 
@@ -177,10 +158,19 @@ class HelloWindow < FXMainWindow
       extras_page = FXVerticalFrame.new(tabbook,:opts => FRAME_RAISED|LAYOUT_FILL)
     end
     
-    def get_select_rows(table)
-      table.selEndRow - table.selStartRow
+    def get_select_rows()
+      self.table.selEndRow - self.table.selStartRow
     end
     
+    def set_table_data(data_table)
+      @row_num = data_table.rows_count
+      (0..data_table.rows_count-1).each do |i|
+        (0..data_table.cols_count-1).each do |j|
+          self.table.setItemText(i,j,data_table.get_item(i,j).to_s)
+        end
+      end
+    end
+
     def check_combobox(combobox,text)
       combobox.connect(SEL_COMMAND) do |sender, sel, index|
         text.editable = false if sender.currentItem !=1
@@ -188,28 +178,9 @@ class HelloWindow < FXMainWindow
       end      
     end
 
-    def update_student_count(count)
-      @count_student = count
-      #здесь надо отображение page_label сделать
-    
-    end
 
-    def datalist_changed(table)
-      row_number = 0 
-      table.each do |row|
-        (1..3).each do |index|
-          table.setItemText(row_number,index-1,row[index].to_s)
-        end
-        row_number+=1
-      end
-    end  
     def create
       super
       show(PLACEMENT_SCREEN)
     end
 end
-
-app = FXApp.new
-HelloWindow.new(app)
-app.create
-app.run
